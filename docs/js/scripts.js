262beca91da08b61de3585bbdba84c8d27864790
AllwynThomas <aet@njit.edu>
2026-02-09 22:49:25 -0500

    feat: implement dark mode toggle functionality using JavaScript with button interaction

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+const button = document.getElementById("myButton");
+const message = document.getElementById("message");
+
+if (button && message){
+    button.addEventListener("click", () => {
+        if (message.textContent === "Current Mode: Light Mode"){
+            message.textContent = "Current Mode: Dark Mode";
+            button.textContent = "Light Mode"
+        }
+        else {
+            message.textContent = "Current Mode: Light Mode";
+            button.textContent = "Dark Mode"
+        }
+        document.body.classList.toggle("darkMode");
+    });
+}
